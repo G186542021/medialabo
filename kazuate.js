34004d3f5f19bf2fae6d34ba8f10220a1007d84c
G186542021 <218654@st.takushoku-u.ac.jp>
2022-07-20 23:49:10 +0900

    7/20

--- kazuate.js
+++ kazuate.js
@@ -1,6 +1,3 @@
-let b = document.querySelector('#print');
-b.addEventListener('click',hantei);
-
 // 答え
 let kotae = Math.floor(Math.random()*10) + 1;
 console.log('答え: ' + kotae);      // デバッグ用
@@ -8,26 +5,19 @@ console.log('答え: ' + kotae);      // デバッグ用
 // 入力回数（予想回数）
 let kaisu = 0;
 
-// 予想を4回実行する
 // 将来: ボタンを押したら， hantei() を呼び出すように修正する
-// hantei();
-
-
-// let result = document.querySelector('#result');
-// let p = document.createElement('p');
+let b = document.querySelector('#print');
+b.addEventListener('click',hantei);
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
-    let i = document.querySelector('input[name="hantei"]');
+    let i = document.querySelector('input[name="seisu"]');
     let h = i.value;
     let yoso = Number(h);
 
     let result = document.querySelector('#result');
     let p = document.createElement('p');
     // 第5回課題:テキストボックスの数値をここに代入
-    // 課題3-1：ここの判定処理を作成する．
-    //        ページに表示する方法はまだ習っていないので
-    //        判定結果はコンソールに出力すること
     
     kaisu++;
     
@@ -43,10 +33,8 @@ function hantei() {
         p.textContent="まちがい．答えはもっと小さいですよ";
     }
     result.insertAdjacentElement('afterend',p);
-
-    //let s = document.querySelector('span#kaisu');
-    //s.textConent = kaisu;
 }
+
 let s = document.querySelector('span#kaisu');
 s.textConent = kaisu;
 
